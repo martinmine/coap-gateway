cd3c4a60c47f4c54f4c66e14cf8b56d40a44e656
martinmine <martin_mine@hotmail.com>
2017-09-21 16:04:34 +0200

    CoAP - HTTP proxy servers

--- src/main/java/no/ntnu/coap/gateway/App.java
+++ src/main/java/no/ntnu/coap/gateway/App.java
@@ -1,32 +1,13 @@
 package no.ntnu.coap.gateway;
 
-import java.net.URI;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import io.netty.channel.Channel;
-import no.ntnu.coap.gateway.resources.*;
-import org.glassfish.jersey.netty.httpserver.NettyHttpContainerProvider;
-import org.glassfish.jersey.server.ResourceConfig;
+import java.io.IOException;
 
 /**
  * Main entry class for the HTTP server.
  */
 public class App {
-    public static void main(String[] args) {
-        final int portNumber = 8081; // Integer.valueOf(args[0]);
-        final URI BASE_URI = URI.create("http://localhost:" + portNumber + "/");
-
-        try {
-            final ResourceConfig resourceConfig = new ResourceConfig(TextResource.class);
-            final Channel server = NettyHttpContainerProvider.createHttp2Server(BASE_URI, resourceConfig, null);
-
-            Runtime.getRuntime().addShutdownHook(new Thread(server::close));
-            System.out.println("Ready on " + BASE_URI.toString());
-
-            Thread.currentThread().join();
-        } catch (InterruptedException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public static void main(String[] args) throws IOException {
+        //System.out.println("Hello world");
+        new ExampleCrossProxy();
     }
 }
