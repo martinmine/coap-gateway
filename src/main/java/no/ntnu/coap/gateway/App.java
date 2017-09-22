68360a3f9df5266ea49f47eb2ea2eb545f5b5f29
martinmine <martin_mine@hotmail.com>
2017-09-22 12:16:26 +0200

    Proper CLI parsing

--- src/main/java/no/ntnu/coap/gateway/App.java
+++ src/main/java/no/ntnu/coap/gateway/App.java
@@ -1,5 +1,12 @@
 package no.ntnu.coap.gateway;
 
+import org.apache.commons.cli.*;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.proxy.DirectProxyCoapResolver;
+import org.eclipse.californium.proxy.ProxyHttpServer;
+import org.eclipse.californium.proxy.resources.ForwardingResource;
+import org.eclipse.californium.proxy.resources.ProxyHttpClientResource;
+
 import java.io.IOException;
 
 /**
@@ -7,22 +14,71 @@ import java.io.IOException;
  */
 public class App {
     public static void main(String[] args) throws IOException {
-        if (args.length != 2) {
-            System.out.println("Arguments must be [CoAP port] [HTTP port]");
-            return;
-        }
-
-        int coapPort;
-        int httpPort;
+        CommandLineParser parser = new DefaultParser();
+        String mode;
+        Integer coapPort, httpPort;
 
         try {
-            coapPort = Integer.parseInt(args[0]);
-            httpPort = Integer.parseInt(args[1]);
-        } catch (NumberFormatException e) {
-            System.out.println("Port numbers must be integers");
+            final CommandLine cli = parser.parse(getCliOptions(), args);
+
+            mode = cli.getOptionValue("mode");
+            coapPort = Integer.valueOf(cli.getOptionValue("coapPort", "5683"));
+            httpPort = Integer.valueOf(cli.getOptionValue("httpPort", "8080"));
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
             return;
         }
 
-        new ExampleCrossProxy(coapPort, httpPort);
+        final CustomProxyCoapClientResource coap2coap = new CustomProxyCoapClientResource("coap2coap");
+
+        if (mode.equals("http")) {
+            System.out.println("Starting HTTP gateway on port " + httpPort);
+
+            final ProxyHttpServer httpServer = new ProxyHttpServer(httpPort);
+            httpServer.setProxyCoapResolver(new DirectProxyCoapResolver(coap2coap));
+        } else if (mode.equals("coap")) {
+            System.out.println("Starting CoAP gateway on port " + coapPort);
+
+            final ForwardingResource coap2http = new ProxyHttpClientResource("coap2http");
+
+            // Create CoAP Server on PORT with proxy resources form CoAP to CoAP and HTTP
+            final CoapServer targetServerA = new CoapServer(coapPort);
+            targetServerA.add(coap2coap);
+            targetServerA.add(coap2http);
+            targetServerA.start();
+        }
+    }
+
+    private static Options getCliOptions() {
+        final Options options = new Options();
+
+        options.addOption(Option.builder("mode")
+                .hasArg()
+                .longOpt("mode")
+                .desc("Whether the proxy should be listening for CoAP or HTTP requests")
+                .required()
+                .build());
+
+        options.addOption(Option.builder("coapPort")
+                .hasArg()
+                .longOpt("coapPort")
+                .desc("UDP port to listen for CoAP requests")
+                .type(Integer.class)
+                .build());
+
+        options.addOption(Option.builder("httpPort")
+                .hasArg()
+                .longOpt("httpPort")
+                .desc("Port to listen for HTTP requests")
+                .type(Integer.class)
+                .build());
+
+        options.addOption(Option.builder("proxyPass")
+                .hasArg()
+                .longOpt("proxyPass")
+                .desc("Where to forward requests")
+                .build());
+
+        return options;
     }
 }
